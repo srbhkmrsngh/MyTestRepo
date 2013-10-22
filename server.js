4aa3985c433c2809c9fadf3cb736592e024799f0
vickeeyz <vickeeyz@laptop.(none)>
2013-10-22 18:39:05 +0530

    corrected the identetion.

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-var WebSocketServer = require('ws').Server
-  , http = require('http')
-  , express = require('express')
-  , app = express()
-  , port = process.env.PORT || 5000;
+var WebSocketServer = require('ws').Server,
+http = require('http'),
+express = require('express'),
+app = express(),
+port = process.env.PORT || 5000;
 
 app.use(express.static(__dirname + '/'));
 
